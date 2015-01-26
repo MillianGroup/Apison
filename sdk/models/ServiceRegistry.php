b9270b7ed3a45522e32cab9fce489395cc1c050c
radoslavS <radoslav.straka@caplin.com>
2015-01-26 10:52:51 +0100

    Added singleton functionality to ServiceRegistry

--- sdk/models/ServiceRegistry.php
+++ sdk/models/ServiceRegistry.php
@@ -4,6 +4,19 @@ class ServiceRegistry
 {
     private $services = array();
 
+    public static function instance()
+    {
+        static $inst = null;
+        if ($inst === null) {
+            $inst = new ServiceRegistry();
+        }
+        return $inst;
+    }
+
+    private function __construct()
+    {
+    }
+
     public function registerService($serviceName, Service $serviceObj)
     {
         $services[$serviceName] = $serviceObj;
